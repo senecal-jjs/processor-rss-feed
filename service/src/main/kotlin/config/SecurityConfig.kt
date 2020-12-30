a5556690d12499404016987781029efa395e3305
Jacob Senecal <jsenecal@figure.com>
2020-12-29 21:32:55 -0700

    add http config

--- service/src/main/kotlin/config/SecurityConfig.kt
+++ service/src/main/kotlin/config/SecurityConfig.kt
@@ -1,29 +1,69 @@
 package config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.BeanIds
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import security.JwtAuthenticationEntryPoint
+import security.JwtAuthenticationFilter
+import security.JwtBuilder
 import security.RssUserDetailsService
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 open class SecurityConfig(
-    private val rssUserDetailsService: RssUserDetailsService
+    private val rssUserDetailsService: RssUserDetailsService,
+    private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    private val jwtBuilder: JwtBuilder,
+    private val unauthorizedHandler: JwtAuthenticationEntryPoint
 ) : WebSecurityConfigurerAdapter() {
+    @Bean
+    open fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
+        return JwtAuthenticationFilter(jwtBuilder, rssUserDetailsService)
+    }
 
     @Bean
     open fun passwordEncoder(): PasswordEncoder? {
         return BCryptPasswordEncoder()
     }
 
-    override fun configure(http: HttpSecurity?) {
-        super.configure(http)
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth
+            .userDetailsService(rssUserDetailsService)
+            .passwordEncoder(passwordEncoder())
+
+        super.configure(auth)
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http
+            .cors()
+            .and()
+            .csrf().disable()
+            .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/v1/auth/**").permitAll()
+            .antMatchers("/api/v1/user/checkUsernameAvailability", "/api/user/checkEmailAvailability").permitAll()
+            .anyRequest().authenticated()
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
     }
 }
